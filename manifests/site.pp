5b7330e84dfda71cce83bf4b5960b0593f25433d
David Lopez <84160227+davids-lab@users.noreply.github.com>
2023-03-08 21:35:44 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,14 @@
  node default {
-    
- }
+  }
  
  node david.master.vm {
    include role::master_server
  }
 
 node /^web/ {
-  include role::app_server
-  }
+   include role::app_server
+}
   
 node /^db/ {
   include role::db_server
-  }
+}
