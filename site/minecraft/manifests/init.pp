fcd607f5deb0d02374d1e9cd61625db6ddd6f0ca
David Lopez <84160227+davids-lab@users.noreply.github.com>
2023-03-25 23:24:36 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,19 +1,19 @@
 class minecraft {
-  file {'/opt/minecraft':  #Creates minecraft directory
+  file {'/opt/minecraft': 
     ensure => directory,
   }
-  file {'/opt/minecraft/server.jar': #download minecraft server file
+  file {'/opt/minecraft/server.jar': 
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar'
   }
-  file {'/opt/minecraft/jdk-17.0.6_linux-x64_bin.rpm':  #installs JAVA17
+  file {'/opt/minecraft/jdk-17.0.6_linux-x64_bin.rpm': 
     ensure   => file,
     source  => 'https://download.oracle.com/java/17/archive/jdk-17.0.6_linux-x64_bin.rpm',
   }
-    package {'java':  #Makes sure JAVA is installed
+    package {'java': 
     ensure => present,
   }
-  file {'/etc/systemd/system/minecraft.service': #Creates file as a service
+  file {'/etc/systemd/system/minecraft.service': 
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
