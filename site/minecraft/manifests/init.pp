a3abf66bda29d11f709c3b3fa420935361e90390
David Lopez <84160227+davids-lab@users.noreply.github.com>
2023-03-25 23:31:07 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,21 +4,21 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar': 
     ensure => file,
-    source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar'
+    source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
   }
   file {'/opt/minecraft/jdk-17.0.6_linux-x64_bin.rpm': 
     ensure   => file,
     source  => 'https://download.oracle.com/java/17/archive/jdk-17.0.6_linux-x64_bin.rpm',
   }
-    package {'java': 
+  package {'java': 
     ensure => present,
+  }
+  file {‘/opt/minecraft/eula.txt’:
+    ensure  => file,
+    content => ‘eula=true’
   }
   file {'/etc/systemd/system/minecraft.service': 
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
-  }
-  service {'minecraft':
-    ensure => running
-    enable => true,
   }
 }
